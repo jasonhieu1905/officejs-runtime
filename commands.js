23987dc9946edd537ccfbb2752711f5950dc9db0
Hieu Doan <doanconghieu92@gmail.com>
2025-06-24 00:08:58 +0700

    Update commands.js

--- commands.js
+++ commands.js
@@ -10,28 +10,42 @@ Office.onReady(() => {
 });
 
 async function changeHeader(event) {
-   Word.run(async (context) => {
+  Word.run(async (context) => {
     const body = context.document.body;
     body.load("text");
     await context.sync();
     const header = context.document.sections.getFirst().getHeader(Word.HeaderFooterType.primary);
-      const firstPageHeader = context.document.sections.getFirst().getHeader(Word.HeaderFooterType.firstPage);
-      header.clear();
-      firstPageHeader.clear();
+    const firstPageHeader = context.document.sections.getFirst().getHeader(Word.HeaderFooterType.firstPage);
+    header.clear();
+    firstPageHeader.clear();
 
-      let xml = '';
-      try {
-        xml = await getCustomXmlPart();
-        header.insertParagraph(xml, "Start");
-      } catch(error) {
-         header.insertParagraph("some error" + JSON.stringify(error), "Start");
-      }
+    let xml = "";
+    try {
+      xml = await getCustomXmlPart();
+      header.insertParagraph(xml, "Start");
+    } catch (error) {
+      header.insertParagraph("some error" + JSON.stringify(error), "Start");
+    }
+
+    header.font.color = "#07641d";
+
     
-      header.font.color = "#07641d";
-      await context.sync();
-  });
+    try {
+      const headerRange = header.getRange();
 
-  await insertContentControlHtml();
+      const cc = headerRange.insertContentControl();
+      cc.tag = "helloWorldHeaderCC";
+      cc.title = "Hello World Header Control";
+      cc.appearance = "BoundingBox";
+
+      cc.insertHtml("<h3>Hello World</h3>", Word.InsertLocation.replace);
+      await context.sync();
+    } catch (error) {
+      console.error("Error inserting html content:", error);
+    }
+
+    await context.sync();
+  });
 
   // Calling event.completed is required. event.completed lets the platform know that processing has completed.
   event.completed();
@@ -50,8 +64,7 @@ async function paragraphChanged() {
       font.color = "#07641d";
 
       await context.sync();
-    }
-    else {
+    } else {
       const header = context.document.sections.getFirst().getHeader(Word.HeaderFooterType.primary);
       header.clear();
       header.insertParagraph("High Confidential - The data must be secret or in some way highly critical", "Start");
@@ -74,14 +87,13 @@ function getGlobal() {
   return typeof self !== "undefined"
     ? self
     : typeof window !== "undefined"
-    ? window
-    : typeof global !== "undefined"
-    ? global
-    : undefined;
+      ? window
+      : typeof global !== "undefined"
+        ? global
+        : undefined;
 }
 
 const g = getGlobal();
-
 
 async function getCustomXmlPart() {
   return new Promise<string | undefined>((resolve, reject) => {
@@ -111,30 +123,6 @@ async function getCustomXmlPart() {
         });
       }
     );
-  });
-}
-
-async function insertContentControlHtml() {
-  await Word.run(async (context) => {
-     const firstSection = context.document.sections.getFirst();
-    // 2. Get its Primary header
-    const header = firstSection.getHeader("Primary");
-    // 3. Get the full range of that header
-    const headerRange = header.getRange();
-    
-    // 4. Wrap that range in a content control
-    const cc = headerRange.insertContentControl();
-    cc.tag = "helloWorldHeaderCC";
-    cc.title = "Hello World Header Control";
-    cc.appearance = "BoundingBox";
-    
-    // 5. Insert your HTML inside the content control
-    cc.insertHtml("<h3>Hello World</h3>", Word.InsertLocation.replace);
-    
-    await context.sync();
-  })
-  .catch((error) => {
-    console.error("Error inserting HTML:", error);
   });
 }
 
